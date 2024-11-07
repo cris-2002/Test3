62bc4e18e78d7888006239c0d64ddda15b7c494b
cris-2002 <gocongcristyjean07@gmail.com>
2024-11-06 23:09:40 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -3,10 +3,27 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>My First Ubuntu Server PHP Deployment</title>
+	<title>Ubuntu Server-PHP Deployment</title>
 </head>
 <body>
-	<p>My First Ubuntu Server PHP Deployment</p>
+
+	<p>Ubuntu Server-PHP Deployment</p>
 
 </body>
+
+
+<div>
+	<label>First Name:</label>
+	<input type="text" name="firstname">
+</div>
+
+<div>
+	<label>Middle Name:</label>
+	<input type="text" name="middlename">
+</div>
+
+<div>
+	<label>Last Name:</label>
+	<input type="text" name="lastname">
+</div>
 </html>
